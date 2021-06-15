341ac5123edb3ced1bb966b89735cd5e7115edfd
EvaSoltesova <soltesova.e@gmail.com>
2021-06-15 16:02:33 +0200

    Pridane V2__init.sql, nefunguje

---
+++ src/main/resources/db/migration/V2__init.sql
@@ -0,0 +1,12 @@
+INSERT INTO post (SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED)
+VALUES ('7-odradzajucich-receptov', 'Lenivá kuchárka', '7 ODRÁDZAJÚCICH RECEPTOV, KTORÉ ZVLÁDNEŠ ĽAVOU ZADNOU!','<p>Toto je 7 mojich receptov</p>','<p>Toto je 7 mojich receptov, ktoré sa na prvý pohľad môžu javiť ako náročné, no opak je pravdou.</p>', '2021-06-05');
+SET
+post_id = IDENTITY();
+INSERT INTO post (SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED)
+VALUES ('radosti-z-trhu','Lenivá kuchárka','RADOSTI Z TRHU','<p>Miesto, kde naplno precítite čerstvosť jednotlivých ingrediencií</p>', 'p>Miesto, kde naplno precítite čerstvosť jednotlivých ingrediencií, zaplníte košík a odchádzate s pocitom radostného naplnenia.</p>', '2021-06-01' );
+SET
+post_id = IDENTITY();
+INSERT INTO post (SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED)
+VALUES ('kolac-bez-muky-a-cukru','Lenivá kuchárka', 'Koláč bez múky a cukru', 'Koláč', 'Koláč bez múky', '2021-01-01');
+SET
+post_id = IDENTITY();
